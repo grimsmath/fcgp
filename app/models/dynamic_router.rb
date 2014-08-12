26d5e82a69a16ea29fe5c61f87e35642b95718a9
David King <grimsmath@gmail.com>
2014-08-12 14:15:22 -0400

    Securing database.yml

--- app/models/dynamic_router.rb
+++ app/models/dynamic_router.rb
@@ -1,9 +1,12 @@
+# http://codeconnoisseur.org/ramblings/creating-dynamic-routes-at-runtime-in-rails-4
 class DynamicRouter < ActiveRecord::Base
   def self.load
     FCGP::Application.routes.draw do
-      Page.all.each do |pg|
-        # puts "Routing #{pg.name}"
-        get "/#{pg.name}", :to => "pages#show", defaults: { id: pg.id }
+      if Page.exists?
+        Page.all.each do |pg|
+          # puts "Routing #{pg.name}"
+          get "/#{pg.name}", :to => "pages#show", defaults: { id: pg.id }
+        end
       end
     end
   end
