0a113c7e19d7fbddd50ed6a4ac77e2abbf8df880
David King <grimsmath@gmail.com>
2014-08-14 16:15:20 -0400

    Ready  for beta deployment

--- db/migrate/20140814003500_remove_extra_email_from_member.rb
+++ db/migrate/20140814003500_remove_extra_email_from_member.rb
@@ -2,6 +2,5 @@ class RemoveExtraEmailFromMember < ActiveRecord::Migration
   def change
     remove_column :members, :email_personal
     remove_column :members, :email_alternate
-    remove_column :members, :vendor_id
   end
 end
